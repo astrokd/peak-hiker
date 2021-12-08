b4a66c22a35883f4000433108a072fefdfa06f53
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-12-08 11:36:00 -0800

    modified mdx pages with peakhiker content, created lic, readme, added some images

---
+++ README.md
@@ -0,0 +1,5 @@
+# Peak Hiker
+
+Created with Gatsbyjs gatsby-starter-mdx-basic
+
+Using the gatsby-plugin-mdx tool.
--- src/components/image.js
+++ src/components/image.js
@@ -15,7 +15,7 @@ import Img from 'gatsby-image'
 const Image = () => {
   const data = useStaticQuery(graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "Hikes - 14 of 17.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
