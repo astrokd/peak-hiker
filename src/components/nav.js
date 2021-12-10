c9469a4c0a2083d22c68f2ca97ff89a64f798836
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-12-10 11:52:36 -0800

    created dropdwon nav for current 2 pages

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,18 +4,27 @@ import React from 'react'
 
 import Dropdown from 'react-bootstrap/Dropdown'
 
+const BurgerIcon = () => {
+    return (
+        <svg height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px"><path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2 S29.104,22,28,22z"/>
+        </svg>
+    )
+}
+
 const Nav = () => {
     return (
         <Dropdown style={{
             float: 'right',
           }}>
-            <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
+            <Dropdown.Toggle style={{
+                background: 'white',
+                }}>
+                <BurgerIcon />
             </Dropdown.Toggle>
             <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                <Dropdown.Item><Link to="/">Home</Link></Dropdown.Item>
+                <Dropdown.Item><Link to="/page-2">Page 2</Link></Dropdown.Item>
+                <Dropdown.Item>Something else</Dropdown.Item>
             </Dropdown.Menu>
         </Dropdown>
     )
