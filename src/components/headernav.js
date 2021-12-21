b57a5dde94f360d54326f0d9c592b841a2c9ac42
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-12-21 09:55:33 -0800

    added style and activestyle to Links

--- src/components/headernav.js
+++ src/components/headernav.js
@@ -10,12 +10,12 @@ import Container from 'react-bootstrap/Container'
 const Header = ({ siteTitle }) => (
   <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
   <Container>
-  <Navbar.Brand><Link to="/" activeStyle={{ color: "white" }}>{siteTitle}</Link></Navbar.Brand>
+  <Navbar.Brand><Link to="/" style={{ color: "white" }}>{siteTitle}</Link></Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link><Link to="/">Home</Link></Nav.Link>
-      <Nav.Link><Link to="/info">Info</Link></Nav.Link>
+      <Nav.Link><Link to="/" activeStyle={{ color: "red" }} style={{ color: "white" }}>Home</Link></Nav.Link>
+      <Nav.Link><Link to="/info" activeStyle={{ color: "red" }} style={{ color: "white" }}>Info</Link></Nav.Link>
       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
