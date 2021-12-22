ac97f1cb802d1a2bb4102c052016ab5578df8c01
Kevin Dreyer <kdreyerinseattle@gmail.com>
2021-12-22 10:17:35 -0800

    added alt to image to fix error

--- src/components/image.js
+++ src/components/image.js
@@ -3,7 +3,10 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const Image = () => {
     return (
-      <StaticImage src='../images/peak-hikes.png'/>
+      <StaticImage src='../images/peak-hikes.png' 
+        alt="view from a mountain peak"
+        placeholder="blurred"
+      />
     )
 }
 export default Image
