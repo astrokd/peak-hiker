9a61b487d4aafd9ae67ba59731ac7f681f64552a
Kevin Dreyer <kdreyerinseattle@gmail.com>
2022-01-18 10:53:54 -0800

    adding mapbox map component

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,7 +26,13 @@ const Layout = ({ children }) => (
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
             crossOrigin="anonymous"
-            />
+          />
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta
+            name="description"
+            content="Create a React web app that uses Mapbox GL JS to render a map"
+          />
         </Helmet>
         <HeaderNav siteTitle={data.site.siteMetadata.title} />
         <Container>
